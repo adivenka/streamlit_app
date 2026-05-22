5f0ed31ed70be8bb5f4506457a1d94caf265628a
VenkaADI <adivenka@gmail.com>
2026-05-22 12:49:14 +0200

    ajout d'un multiselect

--- myapp.py
+++ myapp.py
@@ -56,3 +56,12 @@ option = st.selectbox(
 
 st.write('Your favorite color is ', option)
 
+st.header('st.multiselect')
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue', 'pink', 'purple', 'orange', 'brown'],
+     ['Green', 'Yellow'])
+
+st.write('You selected:', options)
+
